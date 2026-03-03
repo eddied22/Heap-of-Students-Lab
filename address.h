8b9c4523f1d7e20cacafa36a5d96b1d67ec9d060
eddied22 <eddiemullins55@gmail.com>
2026-03-03 18:16:01 -0500

    Update address.h

--- address.h
+++ address.h
@@ -1,13 +1,26 @@
-//
-// Created by Eddie Mullins on 2/18/26.
-//
+#ifndef ADDRESS_H
+#define ADDRESS_H
+#include <iostream>
+#include <string>
+class Address {
+private:
+    std::string street;
+    std::string city;
+    std::string state;
+    std::string zip;
+public:
+    Address() {}
 
-#ifndef HEAP_OF_STUDENTS_ADDRESS_H
-#define HEAP_OF_STUDENTS_ADDRESS_H
+    Address(std::string st, std::string c, std::string s, std::string z) {
+        street = st;
+        city = c;
+        state = s;
+        zip = z;
+    }
 
-
-class address {
+    void print() {
+        std::cout << street << std::endl;
+        std::cout << city << " " << state << ", " << zip << std::endl;
+    }
 };
-
-
-#endif //HEAP_OF_STUDENTS_ADDRESS_H
+#endif
