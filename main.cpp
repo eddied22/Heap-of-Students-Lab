0b531723133f30b9c4f5e7740fa1bd5acbcdcfaf
eddied22 <eddiemullins55@gmail.com>
2026-03-03 18:11:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,106 @@
 #include <iostream>
-#include "date.h"
-#include "address.h"
-#include "student.h"
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include "Student.h"
 
-void testAddress();
-void testDate();
-void testStudent();
+void loadStudents(std::vector<Student*>& students) {
+    std::ifstream file("students.csv");
+    std::string line;
 
-int main(){
-    std::cout << "Hello!" << std::endl;
-    testAddress();
-    testDate();
-    testStudent();
+    while (getline(file, line)) {
+        Student* s = new Student(line);
+        students.push_back(s);
+    }
+
+    file.close();
+}
+
+void printStudents(std::vector<Student*>& students) {
+    for (auto s : students) {
+        s->printFull();
+    }
+}
+
+void showStudentNames(std::vector<Student*>& students) {
+    for (auto s : students) {
+        s->printName();
+    }
+}
+
+void findStudent(std::vector<Student*>& students) {
+    std::string search;
+    std::cout << "Last name of student: ";
+    std::cin >> search;
+
+    for (auto s : students) {
+        if (s->getLastName().find(search) != std::string::npos) {
+            s->printFull();
+        }
+    }
+}
+
+void deleteStudents(std::vector<Student*>& students) {
+    for (auto s : students) {
+        delete s;
+    }
+    students.clear();
+}
+
+std::string menu() {
+    std::string choice;
+    std::cout << "\n0) quit\n";
+    std::cout << "1) print all student names\n";
+    std::cout << "2) print all student data\n";
+    std::cout << "3) find a student\n";
+    std::cout << "4) sort by last name\n";
+    std::cout << "5) sort by first name\n";
+    std::cout << "6) sort by credit hours (descending)\n";
+    std::cout << "\nplease choose 0-6: ";
+    std::cin >> choice;
+    return choice;
+}
+
+int main() {
+    std::vector<Student*> students;
+    loadStudents(students);
+
+    std::string choice;
+
+    do {
+        choice = menu();
+
+        if (choice == "1") {
+            showStudentNames(students);
+        }
+        else if (choice == "2") {
+            printStudents(students);
+        }
+        else if (choice == "3") {
+            findStudent(students);
+        }
+        else if (choice == "4") {
+            std::sort(students.begin(), students.end(),
+                [](Student* a, Student* b) {
+                    return a->getLastName() < b->getLastName();
+                });
+        }
+        else if (choice == "5") {
+            std::sort(students.begin(), students.end(),
+                [](Student* a, Student* b) {
+                    return a->getFirstName() < b->getFirstName();
+                });
+        }
+        else if (choice == "6") {
+            std::sort(students.begin(), students.end(),
+                [](Student* a, Student* b) {
+                    return a->getCredits() > b->getCredits();
+                });
+        }
+
+    } while (choice != "0");
+
+    deleteStudents(students);
+
     return 0;
-}
-void testAddress(){
-    address a;
-    a.init("123 W Main St", "Muncie", "IN", "47303");
-    a.printAddress();
-}
-void testDate(){
-    date d;
-    d.init("01/27/1997");
-    d.printDate();
-}
-void testStudent(){
-    std::string studentString = "Danielle,Johnson,32181 Johnson Course Apt. 389,New Jamesside,IN,59379,02/17/2004,05/15/2027,65";
-    Student* student = new Student();
-    student->init(studentString);
-    student->printStudent();
-    std::cout << std::endl;
-    std::cout << student->getLastFirst();
-    delete student;
 }
